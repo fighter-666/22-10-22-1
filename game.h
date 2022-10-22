be8c3a0791748dce61e37b890a9316df47cd7009
fighter-666 <115720897+fighter-666@users.noreply.github.com>
2022-10-22 23:05:30 +0800

    Add files via upload

---
+++ game.h
@@ -0,0 +1,15 @@
+ #define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+#define EASY_COUNT 10
+#define ROW 9
+#define COL 9
+
+#define ROWS ROW+2
+#define COLS COL+2
+
+void InitBoard(char board[ROWS][COLS], int rows, int cols, char set);
+void DisplayBoard(char board[ROWS][COLS], int row, int col);
+void SetMine(char mine[ROWS][COLS], int row, int col);
+void FindMine(char mine[ROWS][COLS], char show[ROWS][COLS], int row, int col);
